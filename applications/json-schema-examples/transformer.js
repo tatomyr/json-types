692805830907555d4299250af2ae3b148c29c713
Andrew Tatomyr <andrew.tatomyr@gmail.com>
2026-04-16 17:26:40 +0300

    use bundleFromString to resolve standalone json schemas

--- applications/json-schema-examples/transformer.js
+++ applications/json-schema-examples/transformer.js
@@ -1,27 +1,59 @@
 /* global process */
 /**
- * Transforms a json-schema to x-types (WIP)
+ * Transforms a json-schema to x-types using bundleFromString approach
  *
  * example run: `node applications/json-schema-examples/transformer.js applications/json-schema-examples/blog-post.schema.yaml > applications/json-schema-examples/blog-post.x-type.yaml`
  */
 
-import fs from 'fs'
-import {parseYaml, stringifyYaml} from '@redocly/openapi-core'
-import {translateJSONSchemaToXType} from '../json-schema-adapter.js'
+import path from 'path'
+import {
+  stringifyYaml,
+  bundleFromString,
+  createConfig,
+  loadConfig,
+} from '@redocly/openapi-core'
 
-const transform = jsonSchema => {
-  const ctx = {
-    resolve: node => ({
-      node: {
-        type: 'string',
-        enum: ['$ref:' + node.$ref],
-      },
-    }),
-  }
-  return translateJSONSchemaToXType(jsonSchema, ctx)
+const ROOT_X_TYPE = 'X-Test'
+
+const transformUsingBundle = async fileName => {
+  const jsonSchemaFilePath = path.resolve(fileName)
+
+  // Create a minimal OpenAPI document with a $ref to the schema
+  const openApiDoc = `
+    openapi: '3.1.0'
+    info: 
+      title: 'Schema Transformer'
+      version: '1.0.0'
+    components: 
+      schemas: 
+        ${ROOT_X_TYPE}:
+          $ref: ${jsonSchemaFilePath}
+  `
+
+  // Bundle the OpenAPI document to resolve the $ref and prepare it for transformation
+  const tempBundled = await bundleFromString({
+    source: openApiDoc,
+    config: await createConfig({}),
+  })
+
+  // Bundle using x-type.redocly.yaml config to transform the schema to x-type
+  const configPath = path.resolve(
+    path.dirname(new URL(import.meta.url).pathname),
+    '../generate-x-types.redocly.yaml'
+  )
+  const config = await loadConfig({configPath})
+  const bundledDoc = await bundleFromString({
+    source: JSON.stringify(tempBundled.bundle.parsed),
+    config,
+  })
+
+  // Extract the x-type from bundled components/schemas
+  const xTypes = bundledDoc?.bundle?.parsed?.components?.['x-types']
+
+  return xTypes
 }
 
 const fileName = process.argv[2]
-const content = fs.readFileSync(fileName, 'utf8')
-const transformed = transform(parseYaml(content))
-process.stdout.write(stringifyYaml(transformed))
+const result = await transformUsingBundle(fileName)
+
+process.stdout.write(stringifyYaml(result[ROOT_X_TYPE]))
